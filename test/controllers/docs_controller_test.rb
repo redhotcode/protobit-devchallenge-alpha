b51ad1303d8714badcea82129cfef9f929ca40da
Shane Kamar <shanekamar@mac.com>
2014-07-06 02:02:04 -0500

    Testing: Docs Controller
    - Added a simple test that ensures the readme page displays successfully.

--- test/controllers/docs_controller_test.rb
+++ test/controllers/docs_controller_test.rb
@@ -1,7 +1,11 @@
 require 'test_helper'
 
 class DocsControllerTest < ActionController::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "should display readme successfully" do
+    get :readme
+    assert_response :success, "Wrong response received."
+    assert_template layout: 'layouts/application'
+    assert_equal css_select('.panel-body').length, 3, "Not all panel bodies rendered."
+    assert_equal css_select('.panel-body > h1').length, 3, "Not all markdown documents rendered."
+  end
 end
